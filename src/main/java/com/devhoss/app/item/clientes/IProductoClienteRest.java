8344a0b1199d7e4fdeba53f998cb79e82f54f337
devhoss <velasco2288@gmail.com>
2023-09-11 17:24:10 -0500

    SINC2 - load balancer

--- src/main/java/com/devhoss/app/item/clientes/IProductoClienteRest.java
+++ src/main/java/com/devhoss/app/item/clientes/IProductoClienteRest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.devhoss.app.item.models.Producto;
 
-@FeignClient(name = "servicio-productos",url="localhost:8001")
+@FeignClient(name = "servicio-productos")
 public interface IProductoClienteRest {
 	
 	@GetMapping("/listar")
