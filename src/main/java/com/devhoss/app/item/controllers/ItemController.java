cff95e6d47c1c47ed4482d2b7fb9f7b0bbe70779
devhoss <velasco2288@gmail.com>
2023-09-11 12:43:57 -0500

    integracion con microservciio productos

--- src/main/java/com/devhoss/app/item/controllers/ItemController.java
+++ src/main/java/com/devhoss/app/item/controllers/ItemController.java
@@ -2,18 +2,35 @@ package com.devhoss.app.item.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devhoss.app.item.models.Item;
+import com.devhoss.app.item.services.IItemService;
 
 @RestController
 public class ItemController {
 	
+	
+	@Autowired
+	private IItemService iitemService;
+	
 	@GetMapping("/item")
-	public List<Item> listar(){
+	public List<Item> listarItemVacio(){
 		return List.of();
 	}
 	
+	
+	@GetMapping("/listar")
+	public List<Item> listar(){
+		return iitemService.findAll();
+	}
+	
+	@GetMapping("/ver/{id}/cantidad/{cantidad}")
+	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return iitemService.findById(id, cantidad);
+	}
 	
 }
