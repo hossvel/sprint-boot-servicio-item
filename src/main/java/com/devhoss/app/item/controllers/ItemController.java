6819ceb596774fe979b946e2d9fe5104ce65eb74
devhoss <velasco2288@gmail.com>
2023-09-11 19:21:39 -0500

    SINC - histric-resilenc4j

--- src/main/java/com/devhoss/app/item/controllers/ItemController.java
+++ src/main/java/com/devhoss/app/item/controllers/ItemController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 import com.devhoss.app.item.models.Item;
+import com.devhoss.app.item.models.Producto;
 import com.devhoss.app.item.services.IItemService;
 
 @RestController
@@ -31,8 +33,21 @@ public class ItemController {
 	}
 	
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
+	@CircuitBreaker(name = "detalle", fallbackMethod = "metodoAlternativo")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return iitemService.findById(id, cantidad);
 	}
 	
+	
+	public Item metodoAlternativo(Throwable throwable) {
+		Item item = new Item();
+		Producto producto = new Producto();
+		
+		item.setCantidad(1);
+		producto.setId(1l);
+		producto.setNombre("Producto por defecto");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		return item;
+	}
 }
