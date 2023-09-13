0bc6e8f097bb1d6946ff1c86737a787b9c5aa9ed
devhoss <velasco2288@gmail.com>
2023-09-13 13:55:33 -0500

    creando un metodo rest api para obtener la configuracion

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sprint-boot-servicio-item
+
+# Prod: http://localhost:8007/obtener-config
+# DEV: http://localhost:8005/obtener-config
--- src/main/java/com/devhoss/app/item/controllers/ItemController.java
+++ src/main/java/com/devhoss/app/item/controllers/ItemController.java
@@ -1,16 +1,22 @@
 package com.devhoss.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,11 +41,18 @@ public class ItemController {
 	@Qualifier("serviceFeign")
 	private IItemService iitemService;
 	
+	
+	@Autowired
+	private Environment env;
+	
+	
 	@GetMapping("/item")
 	public List<Item> listarItemVacio(){
 		return List.of();
 	}
 	
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	@GetMapping("/listar")
 	public List<Item> listar(@RequestParam(name="nombre", required= false) String nombre, @RequestHeader(name="token-request", required = false) String token){
@@ -109,4 +122,21 @@ public class ItemController {
 		return CompletableFuture.supplyAsync(()-> item);
 	}
 	
+
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+		
+		logger.info(texto);
+		
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
 }
